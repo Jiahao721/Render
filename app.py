bbc7a92ea2b99f185e70c11a8051b60a5c09c06b
Leadon <jil558@ucsd.edu>
2025-08-27 08:02:12 +0000

    Update welcome message

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print('updated and deployed using Render!')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'This app was updated and deployed using Render!'
+
+
 
